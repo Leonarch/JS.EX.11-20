f2f62b1b94e868b396493af8c7dfc73ebcee9308
Leonarch <arcomat@virgilio.it>
2021-09-12 12:27:12 +0200

    Java Upadate3

---
+++ exercise-3.js
@@ -0,0 +1,21 @@
+const sum = (a, b) => a + b; 
+
+
+const subtract = (a, b) => a - b;
+
+
+const multiply = (a, b) =>  a * b;
+
+
+const divide = (a, b) => a / b;
+
+
+const log = (value) => console.log(value);
+
+// sum (2,4);
+// sum (5,2);
+// multiply (sum (2,4), sum(5,2));
+// subtract (multiply(sum (2,4), sum(5,2)),2);
+// divide ((subtract (multiply(sum (2,4), sum(5,2)),2)),5);
+
+log (divide ((subtract (multiply(sum (2,4), sum(5,2)),2)),5));
