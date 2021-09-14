d107f50fa766f4dc51eb4d11b64e69bc5fd50370
Leonarch <arcomat@virgilio.it>
2021-09-14 17:05:14 +0200

    Java Update8

---
+++ exercise-17.js
@@ -0,0 +1,31 @@
+function calculate() {
+let operation = 0;
+
+return {
+  add(n) {
+    operation += n;
+    return this;
+  },
+
+  sub(n) {
+    operation -= n;
+    return this;
+  },
+
+  multiply(n) {
+    operation *= n;
+    return this;
+  },
+
+  divide(n) {
+    operation /= n;
+    return this;
+  },
+
+  printResult(){
+  console.log(operation);}
+}
+}
+
+const calculator = calculate();
+calculator.add(2).add(4).multiply(3).sub(1).sub(3).divide(2).printResult(); // Il risultato sarà: 7
