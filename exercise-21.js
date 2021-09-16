b87ea9e85debe106183fc027d5415a86353cb341
Leonarch <arcomat@virgilio.it>
2021-09-16 16:50:09 +0200

    Java Update12

--- exercise-21.js
+++ exercise-21.js
@@ -2,14 +2,14 @@ const person1 = {
   firstName: 'John',
   lastName: 'Doe',
   age: 25,
-  address: {
+  address: 
     state: 'Italy',
     region: 'Lazio',
     city: 'Rome'
   }
 };
 
-const person2 = JSON.spare(JSON.stringify(person1);
+const person2 = JSON.parse(JSON.stringify(person1);
 
 person2.address.region = 'Lombardia';
 person2.address.city = 'Milan';
